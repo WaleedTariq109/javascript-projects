93ccc8e08c8d64af981268937537074cdb0c3268
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-19 04:01:41 +0500

    Generate Random number

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting HTML Elements
+const number = document.querySelector('.number');
+const chechBtn = document.querySelector('.check');
+const message = document.querySelector('.message');
+
+// Generate Random number between 1 to 20
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+number.textContent = randomNumber;
